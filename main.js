a7b59bc8e6d5e49465276bf21d82f9fb7a18f9d0
abderahmenabdou <ettaharmammar@outlook.com>
2024-08-12 05:44:33 +0100

    Update

--- main.js
+++ main.js
@@ -1,90 +1,93 @@
 let amount = document.getElementById("amount");
 let years = document.getElementById("years");
 let interest = document.getElementById("interest");
-let btn = document.querySelector(".btn");
-let result = document.getElementById("result")
-let repayment = document.getElementById("repayment")
-let interestOnly = document.getElementById('interest-only')
+
+let result = document.getElementById("result");
+let repayment = document.getElementById("repayment");
+let interestOnly = document.getElementById("interest-only");
 
 let errorAmount = document.getElementById("error-amount");
 let errorYears = document.getElementById("error-years");
 let errorInterest = document.getElementById("error-interest");
 let errorMortgage = document.getElementById("error-mortgage");
 
-btn.addEventListener("click", () => {
+let myForm = document.getElementById("myForm");
 
+myForm.addEventListener("submit", (event) => {
+    event.preventDefault()
     let isValid = true;
-    if(amount.value === ""){
-        errorAmount.textContent = "Field must be filled"
-        errorAmount.style.color = "red"
-        isValid = false
-    }else{
-        errorAmount.textContent = ""
-        errorAmount.style.color = ""
-    }
-    
-    if(years.value === ""){
-        errorYears.textContent = "Field must be filled"
-        errorYears.style.color = "red"
-        isValid = false
-    }else{
-        errorYears.textContent = ""
-        errorYears.style.color = ""
-    }
-    
-    if(interest.value === ""){
-        errorInterest.textContent = "Field must be filled"
-        errorInterest.style.color = "red"
-        isValid = false
-    }else{
-        errorInterest.textContent = ""
-        errorInterest.style.color = ""
-    }
-    
-    if(repayment.checked && interestOnly.checked){
-        errorMortgage.textContent = "You must choose only one"
-        errorMortgage.style.color = "red"
-        isValid = false
-    }else if(!repayment.checked && !interestOnly.checked){
-        errorMortgage.textContent = "You must choose only one"
-        errorMortgage.style.color = "red"
-        isValid = false
-    }else{
-        errorMortgage.textContent = ""
-        errorMortgage.style.color = ""
-    }
-    
-    if(!isValid){
-        return
+    if (amount.value === "") {
+      errorAmount.textContent = "Field must be filled";
+      errorAmount.style.color = "red";
+      isValid = false;
+    } else {
+      errorAmount.textContent = "";
+      errorAmount.style.color = "";
     }
 
-showData()
-clearAll()
-});
+    if (years.value === "") {
+      errorYears.textContent = "Field must be filled";
+      errorYears.style.color = "red";
+      isValid = false;
+    } else {
+      errorYears.textContent = "";
+      errorYears.style.color = "";
+    }
 
-function showData(){
-    let loan = Number(amount.value);
-let loanTerm = Number(years.value);
-let interestRate = Number(interest.value / 100);
+    if (interest.value === "") {
+      errorInterest.textContent = "Field must be filled";
+      errorInterest.style.color = "red";
+      isValid = false;
+    } else {
+      errorInterest.textContent = "";
+      errorInterest.style.color = "";
+    }
 
-//   monthly interest rate
-let monthlyInterestRate = interestRate / 12
+    if (repayment.checked && interestOnly.checked) {
+      errorMortgage.textContent = "You must choose only one";
+      errorMortgage.style.color = "red";
+      isValid = false;
+    } else if (!repayment.checked && !interestOnly.checked) {
+      errorMortgage.textContent = "You must choose only one";
+      errorMortgage.style.color = "red";
+      isValid = false;
+    } else {
+      errorMortgage.textContent = "";
+      errorMortgage.style.color = "";
+    }
 
-// the total number of monthly payments
-let numberOfPayments = loanTerm * 12
+    if (!isValid) {
+      return;
+    }
 
-// monthly installment
+    showData();
+    clearAll();
+  });
 
-let expansion = monthlyInterestRate * (1 + monthlyInterestRate)**numberOfPayments
+function showData() {
+  let loan = Number(amount.value);
+  let loanTerm = Number(years.value);
+  let interestRate = Number(interest.value / 100);
 
-let rank = (1 + monthlyInterestRate)**numberOfPayments - 1
+  //   monthly interest rate
+  let monthlyInterestRate = interestRate / 12;
 
-let installment = Number((loan * (expansion / rank)).toFixed(2))
+  // the total number of monthly payments
+  let numberOfPayments = loanTerm * 12;
 
-// Total amount paid
-let totalAmountPaid = installment * numberOfPayments
+  // monthly installment
 
-if(repayment.checked){
+  let expansion =
+    monthlyInterestRate * (1 + monthlyInterestRate) ** numberOfPayments;
+
+  let rank = (1 + monthlyInterestRate) ** numberOfPayments - 1;
+
+  let installment = Number((loan * (expansion / rank)).toFixed(2));
+
+  // Total amount paid
+  let totalAmountPaid = installment * numberOfPayments;
+
+  if (repayment.checked) {
     result.innerHTML = `
             <div class="text-content">
                 <h3>your results</h3>
@@ -100,10 +103,10 @@ if(repayment.checked){
                     <h2>$${totalAmountPaid}</h2>
                 </div>
             </div>
-`
-}
+`;
+  }
 
-if(interestOnly.checked){
+  if (interestOnly.checked) {
     result.innerHTML = `
     <div class="text-content">
         <h3>your results</h3>
@@ -115,13 +118,12 @@ if(interestOnly.checked){
             <h1>$${installment}</h1>
         </div>
     </div>
-`
-}
+`;
+  }
 }
 
-
-function clearAll(){
-    amount.value = ""
-    years.value = ""
-    interest.value = ""
+function clearAll() {
+  amount.value = "";
+  years.value = "";
+  interest.value = "";
 }
